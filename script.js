3f85e4a122a17ca5beaed2c73d76a9d53f11315e
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-18 22:32:22 +0500

    update

--- script.js
+++ script.js
@@ -1010,15 +1010,7 @@
 
 
 
-for (let i = 1; i <=13; i++) {
-    
-    for (let j = 1; j <=10; j++) {
-        console.log(`${i}*${j}=${i*j}`);
-        
-        
-    }
-      console.log('-----------------------------');
-}
+let str = "hello"
 
 
 
