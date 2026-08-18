655003debff8066127aa0cf4b6002f27e6414718
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-18 21:36:12 +0500

    update

--- script.js
+++ script.js
@@ -1012,9 +1012,11 @@
 for (let i = 0; i <=5; i++) {
     let row = '';
     for (let j = 0; j <=i; j++) {
-        row +=
+        row += '*';
         
     }
+    console.log(row);
+    
     
 }
 
