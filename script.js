ebee2e6cab1a2d0842924060371be83d2b29dbb3
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-18 22:39:25 +0500

    update

--- script.js
+++ script.js
@@ -1009,12 +1009,18 @@
 
 
 
+//homework
+// let str = "hello";
+// for (let i = str.length-1; i >=0; i--) {
+//     console.log(str[i]);  
+// }
 
-let str = "hello";
 
-for (let i = str.length-1; i >=0; i--) {
-    console.log(str[i]);  
-}
+
+
+
+
+//2-misol
 
 
 
