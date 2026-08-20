b32c00d2a09e0a1e02d0f65897d49ae52a278430
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-20 17:40:38 +0500

    update

--- script.js
+++ script.js
@@ -1065,20 +1065,18 @@
 
 
 
-//5-misol
-// let str = "hello world";
-// let result = "";
-// for (let i = 0; i < str.length; i++) {
-//     if (i === 0) {
-//         result += str[i].replaceAll("h", "H");
-//     } else {
-//         result += str[i];
-//     }
-// }
-// console.log(result);'
+// 5-misol
+let str = "hello world";
+let result = "";
+for (let i = 0; i < str.length; i++) {
+    if (i === 0) {
+        result += str[i].replaceAll("h", "H");
+    } else {
+        result += str[i];
+    }
+}
+console.log(result);
 
-
-   
 
 
 
