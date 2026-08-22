14972767aee0bec701e1153e29d977bff6e40ffb
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-22 20:13:59 +0500

    update

--- script.js
+++ script.js
@@ -984,8 +984,7 @@ console.log(str.replaceAll("s", "S").replace("d", "D"));
 // -----
 let str= "aabbcccddee";
 console.log(str.substring(1,3),str.substring());
-console.log(console.log();
-);
+console.log(console.log());
 
 
 
