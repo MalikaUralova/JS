41054674375fa7fabe03b04c6ff5ad5ff1e54bb5
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-23 10:43:37 +0500

    update

--- script.js
+++ script.js
@@ -1121,9 +1121,9 @@
 
 
 
-let date = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31] ;
-date.pop();
-console.log(date);
+// let date = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31] ;
+// date.pop();
+// console.log(date);
 
 
 
@@ -1132,7 +1132,11 @@ console.log(date);
 
 
 
-
+let mevalar = [] ;
+mevalar.push("tarvuz");
+mevalar.push("qovun");
+mevalar.push("gilos");
+console.log(mevalar);
 
 
 
