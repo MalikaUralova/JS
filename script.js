75ff13b3dbfeb9ac9270883485faa5bc12afa2d9
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-23 10:44:37 +0500

    update

--- script.js
+++ script.js
@@ -1132,13 +1132,13 @@
 
 
 
-let mevalar = [] ;
-mevalar.push("tarvuz");
-console.log(mevalar);
-mevalar.push("qovun");
-console.log(mevalar);
-mevalar.push("gilos");
-console.log(mevalar);
+// let mevalar = [] ;
+// mevalar.push("tarvuz");
+// console.log(mevalar);
+// mevalar.push("qovun");
+// console.log(mevalar);
+// mevalar.push("gilos");
+// console.log(mevalar);
 
 
 
