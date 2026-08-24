f1a9450e20d19ebaf89e31b160b2f7b905540bea
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-24 19:28:36 +0500

    update

--- script.js
+++ script.js
@@ -1419,10 +1419,10 @@
 
 
 //23-misol
-let num1 = ["oysha", "asal", "sabina", "ilhom", "mahroj", "zokir"];
-let num2 = ["madina", "gulmira", "nafosat", "sardor", "og'abek", "otabek", "temur"];
-let result = num1.concat(num2);
-console.log(result);
+// let num1 = ["oysha", "asal", "sabina", "ilhom", "mahroj", "zokir"];
+// let num2 = ["madina", "gulmira", "nafosat", "sardor", "og'abek", "otabek", "temur"];
+// let result = num1.concat(num2);
+// console.log(result);
 
 
 
