f8dd42cbc3585bc9c2344a58d4058a4dc75e237d
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-25 14:49:27 +0500

    update

--- script.js
+++ script.js
@@ -1476,10 +1476,10 @@
 
 
 //2-qavs orqali [ ] BRACKET NOTATIONS
-let user = {
-    name: "malika",
-    surname: "uralova",
-    age: 16,
-    isDeveloper: true,
-}
-console.log(user["age"]);
+// let user = {
+//     name: "malika",
+//     surname: "uralova",
+//     age: 16,
+//     isDeveloper: true,
+// }
+// console.log(user["age"]);
