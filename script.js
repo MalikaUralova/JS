67acd667e59893efa1de8fe095bb7b811efff072
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-26 12:07:29 +0500

    update

--- script.js
+++ script.js
@@ -1694,11 +1694,6 @@ let savat = [
     }
 ]
 
-
-
-
-
-
 let umumiyNarx = savat.reduce((sum, mahsulot) => sum + mahsulot.narxi * mahsulot.soni, 0)
 console.log(`Savatning umumiy narxi: ${umumiyNarx} so'm`)
 
@@ -1706,7 +1701,7 @@ let pul = 20000
 savat.forEach(mahsulot => {
     let jamiNarx = mahsulot.narxi * mahsulot.soni
     console.log(`${mahsulot.nomi}: ${jamiNarx <= pul ? "sotib olish mumkin" : "pul yetmaydi"}`)
-})
+});
 
 
 
