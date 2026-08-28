eebb23099ca9f380b7394f7168b1c05594d95d65
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-08-28 12:35:04 +0500

    update

--- script.js
+++ script.js
@@ -1850,14 +1850,14 @@
 
 
 //HARAJATNI HISOBLASH
-let cost = {
-    monday: 20000,
-    tuesday: 30000,
-    wednesday: 40000
-}
+// let cost = {
+//     monday: 20000,
+//     tuesday: 30000,
+//     wednesday: 40000
+// }
 
-let total = 0
-for (const i in cost) {
-    total += cost[i]
-}
-console.log(`Umumiy xarajat ${total}`);
+// let total = 0
+// for (const i in cost) {
+//     total += cost[i]
+// }
+// console.log(`Umumiy xarajat ${total}`);
