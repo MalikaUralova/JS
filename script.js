b76d2d2670db9041fa344a33e82ca4b2a25c78fc
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-04 22:02:21 +0500

    update

--- script.js
+++ script.js
@@ -1882,13 +1882,13 @@
 
 
 
-// ISM UZUNLIGI
-// const name = ["Anvar", "Dilnoza", "Ali", "Shahzod", "Bobur"];
-// for (const i of name) {
-//   if (i.length > 5) {
-//     console.log(i);
-//   }
-// }
+ISM UZUNLIGI
+const name = ["Anvar", "Dilnoza", "Ali", "Shahzod", "Bobur"];
+for (const i of name) {
+  if (i.length > 5) {
+    console.log(i);
+  }
+}
 
 
 
