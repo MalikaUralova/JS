4688350094c04d929a3d313589fd4dcfff40cc6d
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-05 23:20:18 +0500

    update

--- script.js
+++ script.js
@@ -1594,21 +1594,21 @@
 
 
 
-// chegirma narx
-// let products = [
-//     {
-//         name: "TV",
-//         price: 5000000,
-//     },
-//     {
-//         name: "laptop",
-//         price: 7000000,
-//     },
-//     {
-//         name: "phone",
-//         price: 4000000,
-//     }
-// ]
+chegirma narx
+let products = [
+    {
+        name: "TV",
+        price: 5000000,
+    },
+    {
+        name: "laptop",
+        price: 7000000,
+    },
+    {
+        name: "phone",
+        price: 4000000,
+    }
+]
 
 // products.map(item => {
 //    console.log(`${item.name} mahsulotiga 10% chegirma beriladi: ${item.price >= 2500000 ? item.price * 0.9 : item.price}`);//
