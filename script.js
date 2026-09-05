9760f5315751c79bdf5eabeb953fb65f4bd302a8
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-05 23:45:06 +0500

    update

--- script.js
+++ script.js
@@ -1499,17 +1499,17 @@
 
 
 
-//OBJECT ICHIDA MASSIV YOZISH
-// let person = {
-//     name: "malika",
-//     skills: ["HTML", "CSS", "JavaScript"],
-//     address: {
-//         city: "Tashkent",
-//         country: "Uzbekistan"
-//     }
-// }
-// console.log(person.name);
-// console.log(person.skills[1]);
+OBJECT ICHIDA MASSIV YOZISH
+let person = {
+    name: "malika",
+    skills: ["HTML", "CSS", "JavaScript"],
+    address: {
+        city: "Tashkent",
+        country: "Uzbekistan"
+    }
+}
+console.log(person.name);
+console.log(person.skills[1]);
 // console.log(person.address.city);
 
 
