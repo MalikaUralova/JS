066dde8f7ab303eecf3f1ec59fce2a9519d2a02a
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-09 20:51:06 +0500

    update

--- script.js
+++ script.js
@@ -1905,29 +1905,29 @@
 
 
 
-const circleBtn = document.getElementById("circleBtn");
-const count = document.getElementById("count");
-const minusBtn = document.getElementById("minusBtn");
-const reset = document.getElementById("reset");
+// const circleBtn = document.getElementById("circleBtn");
+// const count = document.getElementById("count");
+// const minusBtn = document.getElementById("minusBtn");
+// const reset = document.getElementById("reset");
 
-let count2 = 0;
-circleBtn.addEventListener("click", () => {
-    count2++
-    count.textContent = count2
-})
+// let count2 = 0;
+// circleBtn.addEventListener("click", () => {
+//     count2++
+//     count.textContent = count2
+// })
 
 
-minusBtn.addEventListener("click", () => {
-    if (count2 !== 0) {
-        count2--
-        count.textContent = count2
-    }
-})
+// minusBtn.addEventListener("click", () => {
+//     if (count2 !== 0) {
+//         count2--
+//         count.textContent = count2
+//     }
+// })
 
-reset.addEventListener("click", () => {
-    count2 = 0
-    count.textContent = count2
-})
+// reset.addEventListener("click", () => {
+//     count2 = 0
+//     count.textContent = count2
+// })
 
 
 // let count = 0;
