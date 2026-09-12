0ee5e0b353fc0482b629fc9c74660b1343cb10e5
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-12 21:20:38 +0500

    update

--- script.js
+++ script.js
@@ -1722,14 +1722,14 @@
 
 
 
-// FOTBOOL REYTING
-// const teams = [
-//     {
-//         name: "Arsenal",
-//         win: 3,
-//         lose: 2,
-//         tie: 1
-//     },
+FOTBOOL REYTING
+const teams = [
+    {
+        name: "Arsenal",
+        win: 3,
+        lose: 2,
+        tie: 1
+    },
 //     {
 //         name: "Monaco",
 //         win: 7,
@@ -1776,7 +1776,6 @@
 
 
 
-log update .lo
 
 
 
