6fb5215606cbec6f87dca2e95342e81b7e101c22
mahliyouralova880-debug <mahliyouralova880@gmail.com>
2026-09-14 21:52:39 +0500

    update

--- script.js
+++ script.js
@@ -1972,25 +1972,25 @@
 // const openModalBtn = document.getElementById("openModalBtn");
 // const closeModalBtn = document.getElementById("closeModalBtn");
 // const overlay = document.getElementById("overlay");
-const modal = document.getElementById("modal");
-const registerForm = document.getElementById("registerForm");
+// const modal = document.getElementById("modal");
+// const registerForm = document.getElementById("registerForm");
 
-function openModal() {
-  overlay.classList.add("active");
-  modal.classList.add("active");
-}
+// function openModal() {
+//   overlay.classList.add("active");
+//   modal.classList.add("active");
+// }
 
-function closeModal() {
-  overlay.classList.remove("active");
-  modal.classList.remove("active");
-}
+// function closeModal() {
+//   overlay.classList.remove("active");
+//   modal.classList.remove("active");
+// }
 
-openModalBtn.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+// openModalBtn.addEventListener("click", openModal);
+// closeModalBtn.addEventListener("click", closeModal);
+// overlay.addEventListener("click", closeModal);
 
-registerForm.addEventListener("submit", function (e) {
-  e.preventDefault();
+// registerForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
 //   closeModal();
 //   registerForm.reset();
 // });
